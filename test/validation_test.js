76d7c0e89077e963e34132a941c90afb3347de88
Tony Tan <tanxiucai1@gmail.com>
2017-07-22 12:43:37 +0800

    definded validator and test

--- test/validation_test.js
+++ test/validation_test.js
@@ -2,12 +2,33 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Validating records', () => {
-    it('require a user name', () => {
+    it('require a user name', (done) => {
         const user = new User({ name: undefined});
         // const validationResult = user.validateSync();
         user.validate((validationResult) => {
             const { message } = validationResult.errors.name;
             assert(message === "Name is required in UserSchema.");
+            done();
         })
+    });
+
+    it('require username longer than 2 characters', (done) => {
+        const user = new User({ name: 'Al'});
+        user.validate((validationResult) => {
+            const { message } = validationResult.errors.name;
+            assert(message === "Name must be longer than 2 characters.");
+            done();
+        })
+    });
+
+    it('disallow invalid records from being save', (done) => {
+        const user = new User({ name: 'Al'});
+        user.save()
+            .catch((validationResult) => {
+                const { message } = validationResult.errors.name;
+                assert(message  === "Name must be longer than 2 characters.");
+                done();
+            })
+
     });
 });
